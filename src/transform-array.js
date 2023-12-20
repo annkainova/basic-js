c5744251cc7d150be84d1e0cce42a38b650f0ffd
Anna <cainowa@gmail.com>
2023-12-20 12:28:17 +0100

    feat: add 6 task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let transformArray = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === "--discard-next") {
+      i++;
+    } else if (arr[i] === "--double-next") {
+      if (i < arr.length - 1) {
+        transformArray.push(arr[i + 1]);
+      }
+    } else if (arr[i] === "--discard-prev") {
+      if (i > 0 && arr[i - 2] !== "--discard-next") {
+        transformArray.pop();
+      }
+    } else if (arr[i] === "--double-prev") {
+      if (i > 0 && arr[i - 2] !== "--discard-next") {
+        transformArray.push(arr[i - 1]);
+      }
+    } else {
+      transformArray.push(arr[i]);
+    }
+  }
+  return transformArray;
 }
 
 module.exports = {
-  transform
+  transform,
 };
+
+console.log(transform([1, 2, "---double-prev", 3, 9, 4, 5, "--double-next"]));
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getSeason(date) {
+  if (arguments.length === 0) {
+    return "Unable to determine the time of year!";
+  }
+  if (
+    !(date instanceof Date) ||
+    Object.getOwnPropertyNames(date).length > 0 ||
+    Object.prototype.toString.call(date) !== "[object Date]"
+  ) {
+    throw new Error("Invalid date!");
+  }
+
+  let Month = {
+    Jan: "winter",
+    Feb: "winter",
+    Mar: "spring",
+    Apr: "spring",
+    May: "spring",
+    Jun: "summer",
+    Jul: "summer",
+    Aug: "summer",
+    Sep: "autumn",
+    Oct: "autumn",
+    Nov: "autumn",
+    Dec: "winter",
+  };
+  const monthName = date.toLocaleString("en-US", { month: "short" });
+  return Month[monthName];
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
+
